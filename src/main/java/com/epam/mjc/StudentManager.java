28b56bad304da96d0e806c88816e02b1b20c16f9
Nik-kit <nikon12344@mail.com>
2023-04-24 15:32:19 +0400

    hhhh

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,21 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws IllegalArgumentException{
+      Student findStudent = Student.getValueOf(studentID);
+      if(findStudent != null) {
+        return findStudent;
+      } else {
+        throw new IllegalArgumentException("Could not find student with ID " + studentID);
+      }
   }
 
   public static void main(String[] args) {
     StudentManager manager = new StudentManager();
-
     for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+        Student student = manager.find(IDs[i]);
+        System.out.println("Student name " + student.getName());
     }
-
   }
+
 }
